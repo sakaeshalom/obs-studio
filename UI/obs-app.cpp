1def6ad05d72e9ffaf262387dbc1a6ee5850bca8
Sakae Shalom Church <100064296+sakaeshalom@users.noreply.github.com>
2023-06-23 21:41:48 +0900

    UI: Log genlock stats at exit

--- UI/obs-app.cpp
+++ UI/obs-app.cpp
@@ -3488,6 +3488,7 @@ int main(int argc, char *argv[])
 
 	log_blocked_dlls();
 #endif
+	blog(LOG_INFO, "Genlock last offset: %0.3f ms", os_time_compensation_peek_offset_ns() * 1e-6);
 
 	delete_safe_mode_sentinel();
 	blog(LOG_INFO, "Number of memory leaks: %ld", bnum_allocs());
